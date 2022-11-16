592cb3666d63d4336cac9d775d4be77e8a985405
bruno.sasse <bruno.sasse@alunos.sc.senac.br>
2022-11-16 16:54:51 -0300

    ok

---
+++ projeto-php/aula16112022/exercicio-06.php
@@ -0,0 +1,8 @@
+<?php
+
+$color = array (
+    "color" => array("a" => "Red", "b" => "Green", "c" => "White"),
+    "number" => array (1,2,3,4,5,6),
+    "holes" => array ("Fist", 5 => "Second", "Third"));
+
+echo $color;
