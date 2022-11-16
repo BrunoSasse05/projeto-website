be088777474d924315bd9cd027b8ee3c9a56d50d
bruno.sasse <bruno.sasse@alunos.sc.senac.br>
2022-11-16 16:54:55 -0300

    ok

---
+++ projeto-php/aula16112022/exercicio-01.php
@@ -0,0 +1,7 @@
+<?php
+
+$html = '<h3>';
+$html .= 'PHP Tutorial';
+$html .= '</h3>';
+
+echo $html;
---
+++ projeto-php/aula16112022/exercicio-03.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+$html = '<h4>Este é um mal comando para ser executado del ´C:\*.*´</h4>
+            <br>
+        <h4>*Você terá de usar comando de escape para imprimir as aspas;*</h4>';
+
+echo $html;
